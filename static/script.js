3bf2c1dd01282cc2c20fdda71b39a8ed960a3296
Amy Mai <60124518+amymainyc@users.noreply.github.com>
2022-06-07 03:24:47 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -341,7 +341,37 @@ function dino() {
             }, 500)
         }
 
-       
+        let isHuman = setInterval(function () {
+            let barRight = parseInt(window.getComputedStyle(bar).getPropertyValue("width"))
+            if (barRight > 590) {
+                reactionTime = stddev(times)
+                fetch("36")
+                .then(response => response.text())
+                .then(data => {
+                    var content = document.getElementById("dynHTML")
+                    data = data.replace("[reactionTime]", Math.round(reactionTime * 100) / 100)
+                    content.innerHTML = data
+                    var timeoutTime = displayText()
+
+                    setTimeout(function () {
+                        enterText = document.getElementById("enter")
+                        enterText.className = 'visible'
+                        
+                        function continueStory() {
+                            document.removeEventListener('keyup', continueStory)
+                            intro(pageStart+1, pageEnd)
+                        }
+                        document.addEventListener('keyup', continueStory)
+                        document.addEventListener('mouseup', continueStory)
+                    }, timeoutTime)
+                    
+                })
+                .catch(function(error) {
+                    console.log(error)
+                })
+            }
+        }, 1000)
+
     })
     .catch(function(error) {
         console.log(error)
